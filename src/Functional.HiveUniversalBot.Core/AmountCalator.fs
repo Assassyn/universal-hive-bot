a7e3e537aae901aa0b0c47f5cbfbbf29e74902c7
Szymon Sasin <szymon@be-functional.uk>
2023-05-05 18:53:25 +0100

    Added amount calculator

---
+++ src/Functional.HiveUniversalBot.Core/AmountCalator.fs
@@ -0,0 +1,40 @@
+﻿module AmountCalator
+
+open System
+
+let private ZeroWhenNegative value = 
+    if value < 0M
+    then
+        0M
+    else
+        value
+
+let (|StaticValue|_|) (str: string) =
+    let valueWhenLargerThanLimit limit value = 
+        if limit >= value 
+        then 
+            value
+        else
+            limit
+    let mutable limit = 0M
+    if Decimal.TryParse(str, &limit) then Some(valueWhenLargerThanLimit limit)
+    else None
+
+let (|Calculation|_|) (str: string) =
+    if str.Contains ('-') then
+        let numberOfTokensToIgnore = 
+            let tokens = str.Split ('-')
+            tokens.[1]
+        let mutable value = 0M
+        if Decimal.TryParse(numberOfTokensToIgnore, &value) then Some(fun x -> x - value)
+        else None
+    else None
+
+let bind (value: string) =
+    let calculation = 
+        match value with 
+        | StaticValue x -> x
+        | Calculation x -> x
+        | "*" | _  -> (fun amount -> amount)
+
+    calculation >> ZeroWhenNegative
