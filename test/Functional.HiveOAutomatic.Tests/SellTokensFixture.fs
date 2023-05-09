6c09272d269c1ef1632e2d732fc8fb503d41fb19
Szymon Sasin <szymon@be-functional.uk>
2023-05-09 13:15:17 +0100

    added sell token action

---
+++ test/Functional.HiveOAutomatic.Tests/SellTokensFixture.fs
@@ -0,0 +1,34 @@
+﻿module SellTokensFixture
+
+open Xunit
+open FsUnit.Xunit
+open Functional.ETL.Pipeline
+open TestingStubs
+
+let private hiveNodeUrl = "https://anyx.io"
+
+let testData =
+    [|
+        [| ~~123M; ~~"*"; ~~"123" |]
+        [| ~~123M; ~~"100"; ~~"100" |]
+        [| ~~99M; ~~"100"; ~~"99" |]
+    |]
+    
+[<Theory>]
+[<MemberData("testData")>]
+let ``Can sell tokens`` (oneUpBalance:decimal) (amountToBind: string) (result: string) =
+    let reader = UserReader.bind [ ("ultimate-bot", "", "") ]
+    let transformer = 
+        (TestingStubs.mockedBalanceAction [| ("ONEUP", oneUpBalance) |])
+        >> (SellToken.action TestingStubs.logger "ONEUP" (AmountCalator.bind amountToBind))
+    let pipelineDefinition = Pipeline.bind reader transformer
+   
+    let results = processPipeline pipelineDefinition
+    let underTestObject =
+        results
+        |> Seq.collect (fun x-> x.results)
+        |> Seq.item 0
+
+    underTestObject 
+    |> TestingStubs.extractCustomJson 
+    |> should equal (sprintf """{"contractName":"tokens","contractAction":"stake","contractPayload": {"to": "ultimate-bot","symbol": "ONEUP","quantity": "%s"}}""" result)
