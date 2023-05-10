302ecd76f46ab7c968f2830235e8c6c6779ca52d
Szymon Sasin <szymon@be-functional.uk>
2023-05-10 17:54:55 +0100

    removed not used code

--- src/Functional.HiveUniversalBot.Core/Hive.fs
+++ src/Functional.HiveUniversalBot.Core/Hive.fs
@@ -2,40 +2,9 @@
 
 open System.Net.Http
 open HiveAPI
-open System.Text.Json
+open System
 
 type HiveUrl = string 
-
-//type Hive (hiveNodeUrl) = 
-//    let hive = new CHived(new HttpClient(), hiveNodeUrl)
-
-//    let setToUserNameWhenTrue username isTrue =
-//        match isTrue with 
-//        | true -> [| username |]
-//        | _ -> [||]
-
-//    let createCustomJson username activeKey postingKey methodName json = 
-//        new COperations.custom_json (
-//            id = methodName,
-//            json = json,
-//            required_auths = setToUserNameWhenTrue username activeKey,
-//            required_posting_auths = setToUserNameWhenTrue username postingKey)
-    
-//    member this.createCustomJsonActiveKey username methodName json = 
-//        createCustomJson username true false methodName json
-    
-//    member this.createCustomJsonPostingKey username methodName json = 
-//        createCustomJson username false true methodName json
-        
-//    member this.brodcastTransaction operation key = 
-//        hive.broadcast_transaction ([| operation |], [| key |])
-
-//    member this.brodcastTransactions operations key = 
-//        operations
-//        |> Seq.chunkBySize 5
-//        |> Seq.map (fun op -> 
-//            let transactionId = hive.broadcast_transaction (op, [| key |])
-//            transactionId)
 let private setToUserNameWhenTrue username isTrue =
     match isTrue with 
     | true -> [| username |]
@@ -60,4 +29,5 @@ let brodcastTransactions hiveUrl operations key =
     |> Seq.chunkBySize 5
     |> Seq.map (fun op -> 
         let transactionId = hive.broadcast_transaction (op, [| key |])
+        System.Threading.Thread.Sleep (3 |> TimeSpan.FromSeconds)
         transactionId)
