38695a7333542c1b12983baaabd053278717ba17
Szymon Sasin <szymon@be-functional.uk>
2023-06-02 21:21:26 +0100

    added the output of next scheduler action

--- src/Functional.UniversalBot.CLI/Scheduler.fs
+++ src/Functional.UniversalBot.CLI/Scheduler.fs
@@ -24,18 +24,22 @@ let bind logger configuration  =
     |> Seq.iter (fun (cron, action) -> crontab.AddTask(cron, action) |> ignore)
     crontab
 
+let calculateNextOccurance (scheduler: Scheduler) = 
+    let occurences = 
+        scheduler.GetNextOccurrences ()
+        |> Seq.map (fun x -> x.ToTuple())
+        |> Seq.map (fun (date, _) -> date)
+        |> Seq.sortBy (fun x -> x)
+    let dateOfOccurence = occurences |> Seq.head
+    dateOfOccurence
+
+let writeMessageWithNextOccurance scheduler = 
+    let dateOfOccurence = calculateNextOccurance scheduler
+    sprintf "Next action on: %s" (dateOfOccurence.ToString("dd/MM/yyyy HH:mm:ss"))
+
 let start logger (scheduler: Scheduler) = 
     scheduler.Start()
-
-    let message =
-        let occurences = 
-            scheduler.GetNextOccurrences ()
-            |> Seq.map (fun x -> x.ToTuple())
-            |> Seq.map (fun (date, _) -> date)
-            |> Seq.sortBy (fun x -> x)
-        //let nextOccurence = occurences |> Seq.head
-        //let (dateOfOccurence, _) = (nextOccurence.ToTuple()) 
-        let dateOfOccurence = occurences |> Seq.head
-        sprintf "Next action on: %s" (dateOfOccurence.ToString("dd/MM/yyyy HH:mm:ss"))
-        
+    let message = writeMessageWithNextOccurance scheduler         
     logger message
+
+    scheduler.Next.Add (fun _ -> logger (writeMessageWithNextOccurance scheduler))
