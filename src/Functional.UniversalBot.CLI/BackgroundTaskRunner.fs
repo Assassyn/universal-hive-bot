a592f49964104ce5343e02ab27a2347452a0b19d
Szymon Sasin <szymon@be-functional.uk>
2023-06-15 13:41:14 +0100

    added more information to log output to inform

--- src/Functional.UniversalBot.CLI/BackgroundTaskRunner.fs
+++ src/Functional.UniversalBot.CLI/BackgroundTaskRunner.fs
@@ -2,11 +2,15 @@
 
 open FSharp.Control
 open Functional.ETL.Pipeline
+open PipelineResult
 
 let runPipeline logger pipeline = 
-    processPipeline pipeline |> TaskSeq.toArray |> ignore
+    processPipeline pipeline 
+    |> TaskSeq.iter (fun item -> FinishedProcessing item.index |> logger)
+    |> Async.AwaitTask 
+    |> Async.RunSynchronously
 
 let start logger pipelines = 
     pipelines
     |> Seq.filter (fun (config, _) -> Readers.selectContinouseBasedOnly config)
-    |> Seq.iter (fun (_, pipeline) -> runPipeline logger  pipeline)
+    |> Seq.iter (fun (_, pipeline) -> runPipeline logger pipeline)
